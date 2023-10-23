d1e317bbcb05a059c5abae9be67a40b3a83141a5
guseva_nadezhda <gnadya18@gmail.com>
2023-10-23 13:21:26 +0300

    script added

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+const cardName = initialCards.map(function (item) {
+  return item.name;
+}); 
 
-// @todo: DOM узлы
+const cardLink = initialCards.map(function (item) {
+  return item.link;
+}); 
 
-// @todo: Функция создания карточки
+function removeCard(event) {
+  const itemRemove = event.target.closest('.card');
+  itemRemove.remove();
+}
 
-// @todo: Функция удаления карточки
+function addCard(cardValue) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  cardElement.querySelector('.card__title').textContent = cardValue.split('*')[0];
+  cardElement.querySelector('.card__image').alt = cardValue.split('*')[0];;
+  cardElement.querySelector('.card__image').src = cardValue.split('*')[1];;
+  placesList.append(cardElement);
+  console.log(cardElement)
+  deleteButton.addEventListener('click', removeCard);
+};
 
-// @todo: Вывести карточки на страницу
+for (let i = 0; i < cardName.length; i++) {
+  let cardValue = cardName[i] + '*' + cardLink[i];
+  addCard(cardValue);
+}  
